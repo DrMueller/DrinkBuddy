4e807c41a44a53f67f689c4976000534fe937d4b
Müller Matthias <matthias.mueller@noser.com>
2026-04-03 09:21:12 +0200

    WIP

--- Sources/Application/Presentation/Areas/FotoDrinkVorschlag/FotoSituation.cs
+++ Sources/Application/Presentation/Areas/FotoDrinkVorschlag/FotoSituation.cs
@@ -1,15 +1,15 @@
 ﻿namespace DrinkBuddy.Presentation.Areas.FotoDrinkVorschlag
 {
-    public record FotoSituation(string Value, string Name, string Description)
+    public record FotoSituation(string Name, string Description)
     {
         public static IReadOnlyCollection<FotoSituation> CreateAll()
         {
             return new List<FotoSituation>
             {
-                new("PartyGrob", "Grobe Party", "Eine ausgelassene Feier. Shots, Whisky, Gin, Everythign goes"),
-                new("PartyGemuetlich", "Gemütliche Party", "Gemütliches Fest. Bier, weniger starke Shots, Martini etc."),
-                new("Feierabendbier", "Feierabendbier", "Feierabendbier. Bier etc."),
-                new("Apero", "Apero", "Ein Aperitif. Leichte Getränke, Bier etc.")
+                new("Grobe Party", "Eine ausgelassene Feier. Shots, Whisky, Gin, Everythign goes"),
+                new("Gemütliche Party", "Gemütliches Fest. Bier, weniger starke Shots, Martini etc."),
+                new("Feierabendbier", "Feierabendbier. Bier etc."),
+                new("Apero", "Ein Aperitif. Leichte Getränke, Bier etc.")
             };
         }
     }
